7ab3d3e8376ee4bc7d2472af5b6fa7a1e39d5353
bmassioui <bouchaib.massioui@gmail.com>
2022-12-05 22:58:27 +0000

    Initialize xUnit Tests for Integration and Unit

---
+++ 1-src/Catalog.API/Controllers/ApiBaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    public class ApiBaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
