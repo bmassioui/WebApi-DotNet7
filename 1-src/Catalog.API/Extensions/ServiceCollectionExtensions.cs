c51c89eeed76d19fcc5a80d9d3c959d5370eb9fd
bmassioui <bouchaib.massioui@gmail.com>
2022-12-05 23:41:06 +0000

    Add Swagger Open API info and description

---
+++ 1-src/Catalog.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace Catalog.API.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Add Swagger Open API info and description
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddSwaggerOpenApiInfo(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "Catalog API",
+                Description = "An ASP.NET Core Web API for managing Catalog items",
+                TermsOfService = new Uri("https://example.com/terms"),
+                Contact = new OpenApiContact
+                {
+                    Name = "Bouchaib MASSIOUI",
+                    Url = new Uri("https://massioui.vercel.app/")
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "MIT",
+                    Url = new Uri("https://opensource.org/licenses/MIT")
+                }
+            });
+
+            // using System.Reflection;
+            var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+        });
+
+        return services;
+    }
+}
